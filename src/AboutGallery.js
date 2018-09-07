d64018a8f12e374b6703d702312e18569240ac19
David Dunkovich <dunkovichd@yahoo.com>
2018-09-07 12:20:00 -0500

    add beginning project section

--- src/AboutGallery.js
+++ src/AboutGallery.js
@@ -30,44 +30,42 @@ class AboutGallery extends Component {
         },    
     ]
     return (
-        <div>
-            <Grid columns={3} stackable>
-                <Grid.Row>
-                    <Grid.Column style={{padding: '.5rem'}}>
-                        <Reveal animated='small fade'>
-                            <Reveal.Content visible style={{width: '100%'}}>
-                                <Image style={{width: '100%'}} src={images[0].original} alt={images[0].alt}  />
-                            </Reveal.Content>
-                            <Reveal.Content hidden className="width100">
-                                <Image style={{width: '100%'}} src={images[1].original} alt={images[1].alt}  />
-                            </Reveal.Content>
-                        </Reveal>
-                    </Grid.Column>
-                    <Grid.Column style={{padding: '.5rem'}}>
-                        <Reveal animated='small fade' >
-                            <Reveal.Content visible style={{width: '100%'}}>
-                                <Image style={{width: '100%'}} src={images[2].original} alt={images[2].alt}  />
-                            </Reveal.Content>
-                            <Reveal.Content hidden style={{width: '100%'}}>
-                                <Image style={{width: '100%'}} src={images[3].original} alt={images[3].alt}  />
-                            </Reveal.Content>
-                        </Reveal>
-                    </Grid.Column>
-                    <Grid.Column style={{padding: '.5rem'}}>
-                        <Reveal animated='small fade'>
-                            <Reveal.Content visible style={{width: '100%'}}>
+        <Grid columns={3} stackable>
+            <Grid.Row>
+                <Grid.Column style={{padding: '.5rem'}}>
+                    <Reveal animated='small fade'>
+                        <Reveal.Content visible style={{width: '100%'}}>
+                            <Image style={{width: '100%'}} src={images[0].original} alt={images[0].alt}  />
+                        </Reveal.Content>
+                        <Reveal.Content hidden className="width100">
+                            <Image style={{width: '100%'}} src={images[1].original} alt={images[1].alt}  />
+                        </Reveal.Content>
+                    </Reveal>
+                </Grid.Column>
+                <Grid.Column style={{padding: '.5rem'}}>
+                    <Reveal animated='small fade' >
+                        <Reveal.Content visible style={{width: '100%'}}>
+                            <Image style={{width: '100%'}} src={images[2].original} alt={images[2].alt}  />
+                        </Reveal.Content>
+                        <Reveal.Content hidden style={{width: '100%'}}>
+                            <Image style={{width: '100%'}} src={images[3].original} alt={images[3].alt}  />
+                        </Reveal.Content>
+                    </Reveal>
+                </Grid.Column>
+                <Grid.Column style={{padding: '.5rem'}}>
+                    <Reveal animated='small fade'>
+                        <Reveal.Content visible style={{width: '100%'}}>
 
-                                <Image style={{width: '100%'}} src={images[4].original} alt={images[4].alt}  />
-                               
-                            </Reveal.Content>
-                            <Reveal.Content hidden style={{width: '100%'}}>
-                                <Image style={{width: '100%'}} src={images[5].original} alt={images[5].alt} />
-                            </Reveal.Content>
-                        </Reveal>
-                    </Grid.Column>
-                </Grid.Row>
-            </Grid>
-        </div>
+                            <Image style={{width: '100%'}} src={images[4].original} alt={images[4].alt}  />
+                            
+                        </Reveal.Content>
+                        <Reveal.Content hidden style={{width: '100%'}}>
+                            <Image style={{width: '100%'}} src={images[5].original} alt={images[5].alt} />
+                        </Reveal.Content>
+                    </Reveal>
+                </Grid.Column>
+            </Grid.Row>
+        </Grid>     
     );
   }
 }
--- src/Projects.js
+++ src/Projects.js
@@ -1,14 +1,62 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Transition } from 'semantic-ui-react';
+import { Transition, Grid } from 'semantic-ui-react';
+import './Projects.css';
 
 class Projects extends Component {
   render() {
     return (
       <div className="mainContent">
         <Transition visible={this.props.visible} animation='fade right' duration={500}>
-          <h1>projects</h1>
+        <Grid columns={3} stackable>
+            <Grid.Row>
+                <Grid.Column style={{padding: '.5rem'}}>
+                  <figure class="snip1477"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/sample35.jpg" alt="sample35" />
+                    <div class="title">
+                      <div>
+                        <h2>Project</h2>
+                        <h4>Name</h4>
+                      </div>
+                    </div>
+                    <figcaption>
+                      <p>Text about the projectText about the projectText about the project</p>
+                    </figcaption>
+                    <a href="#"></a>
+                  </figure>
+                </Grid.Column>
+                <Grid.Column style={{padding: '.5rem'}}>
+                  <figure class="snip1477"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/sample35.jpg" alt="sample35" />
+                    <div class="title">
+                      <div>
+                        <h2>Project</h2>
+                        <h4>Name</h4>
+                      </div>
+                    </div>
+                    <figcaption>
+                      <p>Text about the project Text about the projectText about the project </p>
+                    </figcaption>
+                    <a href="#"></a>
+                  </figure>
+                </Grid.Column>
+                <Grid.Column style={{padding: '.5rem'}}>
+                  <figure class="snip1477"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/sample35.jpg" alt="sample35" />
+                    <div class="title">
+                      <div>
+                        <h2>Project</h2>
+                        <h4>Name</h4>
+                      </div>
+                    </div>
+                    <figcaption>
+                      <p>Text about the projectText about the projectText about the project</p>
+                    </figcaption>
+                    <a href="#"></a>
+                  </figure>
+                </Grid.Column>
+            </Grid.Row>
+        </Grid>     
         </Transition>
+
+       
       </div>
     );
   }
