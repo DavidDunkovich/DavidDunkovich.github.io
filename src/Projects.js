ba22a1e946ff0aa628c609247b15e0e2ca880f0d
David Dunkovich <dunkovichd@yahoo.com>
2018-09-07 15:54:27 -0500

    project styling

--- src/Projects.js
+++ src/Projects.js
@@ -11,25 +11,25 @@ class Projects extends Component {
         <Grid columns={3} stackable>
             <Grid.Row> 
                 <Grid.Column style={{padding: '.5rem'}}>
-                  <figure class="snip1477"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/sample35.jpg" alt="sample35" />
-                    <div class="title">
+                  <figure className="projectImage"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/sample35.jpg" alt="sample35" />
+                    <div className="title">
                       <div>
-                        <h2>Project</h2>
-                        <h4>Name</h4>
+                        <h2>Website</h2>
+                        <h4>Sure Hope</h4>
                       </div>
                     </div>
                     <figcaption>
-                      <p>Text about the projectText about the projectText about the project</p>
+                      <p>A non-profit organization sponsoring a non-descriminatory school in Uganda.</p>
                     </figcaption>
                     <a href="#"></a>
                   </figure>
                 </Grid.Column>
                 <Grid.Column style={{padding: '.5rem'}}>
-                  <figure class="snip1477"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/sample35.jpg" alt="sample35" />
-                    <div class="title">
+                  <figure className="projectImage"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/sample35.jpg" alt="sample35" />
+                    <div className="title">
                       <div>
                         <h2>Project</h2>
-                        <h4>Name</h4>
+                        <h4>Firestack</h4>
                       </div>
                     </div>
                     <figcaption>
@@ -39,8 +39,8 @@ class Projects extends Component {
                   </figure>
                 </Grid.Column>
                 <Grid.Column style={{padding: '.5rem'}}>
-                  <figure class="snip1477"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/sample35.jpg" alt="sample35" />
-                    <div class="title">
+                  <figure className="projectImage"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/sample35.jpg" alt="sample35" />
+                    <div className="title">
                       <div>
                         <h2>Project</h2>
                         <h4>Name</h4>
