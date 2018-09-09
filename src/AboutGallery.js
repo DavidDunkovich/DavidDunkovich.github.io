e7e46292e01cf4cc8935b48c684696e380470044
David Dunkovich <dunkovichd@yahoo.com>
2018-09-09 18:38:28 -0500

    Added first 3 projects and CSS overhaul

--- src/AboutGallery.js
+++ src/AboutGallery.js
@@ -1,53 +1,53 @@
 import React, { Component } from 'react';
 import { Image, Reveal, Grid  } from 'semantic-ui-react';
+
+const images = [
+    {
+        src: 'images/skiing1.jpg',
+        alt: 'Skiing Image 1',
+    },
+    {
+        src: 'images/skiing2.jpg',
+        alt: 'Skiing Image 2'
+    },
+    {
+        src: 'images/hiking1.jpg',
+        alt: 'Hiking Image 1'
+    },
+    {
+        src: 'images/hiking2.jpg',
+        alt: 'Hiking Image 2'
+    },
+    {
+        src: 'images/traveling1.jpg',
+        alt: 'Traveling Image 2'
+    },
+    {
+        src: 'images/traveling2.jpeg',
+        alt: 'Traveling Image 2'
+    },    
+]
 class AboutGallery extends Component {
     render() {
-
-    const images = [
-        {
-            original: 'images/skiing1.jpg',
-            alt: 'Skiing Image 1',
-        },
-        {
-            original: 'images/skiing2.jpg',
-            alt: 'Skiing Image 2'
-        },
-        {
-            original: 'images/hiking1.jpg',
-            alt: 'Hiking Image 1'
-        },
-        {
-            original: 'images/hiking2.jpg',
-            alt: 'Hiking Image 2'
-        },
-        {
-            original: 'images/traveling1.jpg',
-            alt: 'Traveling Image 2'
-        },
-        {
-            original: 'images/traveling2.jpeg',
-            alt: 'Traveling Image 2'
-        },    
-    ]
     return (
         <Grid columns={3} stackable>
             <Grid.Column style={{padding: '.5rem'}}>
                 <Reveal animated='small fade'>
                     <Reveal.Content visible style={{width: '100%'}}>
-                        <Image style={{width: '100%'}} src={images[0].original} alt={images[0].alt}  />
+                        <Image style={{width: '100%'}} src={images[0].src} alt={images[0].alt}  />
                     </Reveal.Content>
                     <Reveal.Content hidden className="width100">
-                        <Image style={{width: '100%'}} src={images[1].original} alt={images[1].alt}  />
+                        <Image style={{width: '100%'}} src={images[1].src} alt={images[1].alt}  />
                     </Reveal.Content>
                 </Reveal>
             </Grid.Column>
             <Grid.Column style={{padding: '.5rem'}}>
                 <Reveal animated='small fade' >
                     <Reveal.Content visible style={{width: '100%'}}>
-                        <Image style={{width: '100%'}} src={images[2].original} alt={images[2].alt}  />
+                        <Image style={{width: '100%'}} src={images[2].src} alt={images[2].alt}  />
                     </Reveal.Content>
                     <Reveal.Content hidden style={{width: '100%'}}>
-                        <Image style={{width: '100%'}} src={images[3].original} alt={images[3].alt}  />
+                        <Image style={{width: '100%'}} src={images[3].src} alt={images[3].alt}  />
                     </Reveal.Content>
                 </Reveal>
             </Grid.Column>
@@ -55,11 +55,11 @@ class AboutGallery extends Component {
                 <Reveal animated='small fade'>
                     <Reveal.Content visible style={{width: '100%'}}>
 
-                        <Image style={{width: '100%'}} src={images[4].original} alt={images[4].alt}  />
+                        <Image style={{width: '100%'}} src={images[4].src} alt={images[4].alt}  />
                         
                     </Reveal.Content>
                     <Reveal.Content hidden style={{width: '100%'}}>
-                        <Image style={{width: '100%'}} src={images[5].original} alt={images[5].alt} />
+                        <Image style={{width: '100%'}} src={images[5].src} alt={images[5].alt} />
                     </Reveal.Content>
                 </Reveal>
             </Grid.Column>
--- src/Projects.js
+++ src/Projects.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Transition, Grid } from 'semantic-ui-react';
+import { Transition, Grid, Image } from 'semantic-ui-react';
 import './Projects.css';
 //DO i need rows if its set to 3?>
+const images = [
+  {
+      src: 'images/surehope_resized.jpg',
+      alt: 'Sure Hope Foundation Website',
+  },
+  {
+    src: 'images/firestack.jpg',
+    alt: 'Firestack Project',
+  },
+  {
+    src: 'images/shinykeyrealty.png',
+    alt: 'Shiny Key Realty',
+  }         
+]
 class Projects extends Component {
   render() {
     return (
@@ -11,52 +25,53 @@ class Projects extends Component {
         <Grid columns={3} stackable>
             <Grid.Row> 
                 <Grid.Column style={{padding: '.5rem'}}>
-                  <figure className="projectImage"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/sample35.jpg" alt="sample35" />
+                  <figure className="projectImage">
+                    <Image src={images[0].src} alt={images[0].alt} />
                     <div className="title">
-                      <div>
-                        <h2>Website</h2>
-                        <h4>Sure Hope</h4>
+                      <div className="white-bg">
+                        <h3>Website</h3>
+                        <h5>Sure Hope</h5>
                       </div>
                     </div>
                     <figcaption>
-                      <p>A non-profit organization sponsoring a non-descriminatory school in Uganda.</p>
+                      <p>A non-profit organization's website sponsoring a non-descriminatory school in Uganda.</p>
                     </figcaption>
                     <a href="#"></a>
                   </figure>
                 </Grid.Column>
                 <Grid.Column style={{padding: '.5rem'}}>
-                  <figure className="projectImage"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/sample35.jpg" alt="sample35" />
+                  <figure className="projectImage">
+                    <Image src={images[1].src} alt={images[1].alt} />
                     <div className="title">
-                      <div>
-                        <h2>Project</h2>
-                        <h4>Firestack</h4>
+                      <div className="white-bg">
+                        <h3>Web App</h3>
+                        <h5>Firestack</h5>
                       </div>
                     </div>
                     <figcaption>
-                      <p>Text about the project Text about the projectText about the project </p>
+                      <p>First Place/People's Choice awarded hackathon web app used to minimize the amount of time spent on onboarding.</p>
                     </figcaption>
                     <a href="#"></a>
                   </figure>
                 </Grid.Column>
                 <Grid.Column style={{padding: '.5rem'}}>
-                  <figure className="projectImage"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/sample35.jpg" alt="sample35" />
+                  <figure className="projectImage">
+                  <Image src={images[2].src} alt={images[2].alt} />
                     <div className="title">
-                      <div>
-                        <h2>Project</h2>
-                        <h4>Name</h4>
+                      <div className="white-bg">
+                        <h3>Web App</h3>
+                        <h5>Shiny Key Realty</h5>
                       </div>
                     </div>
                     <figcaption>
-                      <p>Text about the projectText about the projectText about the project</p>
+                      <p>Form used to automate emails, collect customer information, and access said information through an admin portal.</p>
                     </figcaption>
                     <a href="#"></a>
                   </figure>
                 </Grid.Column>
             </Grid.Row>
         </Grid>     
-        </Transition>
-
-       
+        </Transition>      
       </div>
     );
   }
