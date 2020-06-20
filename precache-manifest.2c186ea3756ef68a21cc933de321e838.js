aea6cf35f1a66d470beb284ecabd03fe4742ac80
David Dunkovich <dunkovichd@gmail.com>
2020-06-19 17:51:06 -0700

    Updates

---
+++ precache-manifest.2c186ea3756ef68a21cc933de321e838.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8553027faf8d8d2e8d6eed9e64b579c6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "216a5aae2947eb81dfef",
+    "url": "/static/css/main.3156f23d.chunk.css"
+  },
+  {
+    "revision": "5ad096144069f09f3234",
+    "url": "/static/js/2.8ec976f6.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.8ec976f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "216a5aae2947eb81dfef",
+    "url": "/static/js/main.2a8ebfcd.chunk.js"
+  },
+  {
+    "revision": "a79e0ce618c2c4f024c9",
+    "url": "/static/js/runtime-main.30aaf592.js"
+  }
+]);
